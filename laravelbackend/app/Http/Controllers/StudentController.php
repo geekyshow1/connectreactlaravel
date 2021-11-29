1d32a61e39c93d9b0e1002601bcb5abecdd5667a
geekyshows <geekyshows@gmail.com>
2021-11-29 20:44:05 +0530

    first commit

---
+++ laravelbackend/app/Http/Controllers/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index(){
+        return Student::all();
+    }
+}
